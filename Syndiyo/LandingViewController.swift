84e48b7e76600ff51b6128b01538d10b47265b96
Ilham Nurjadin <ilham1400@gmail.com>
2016-07-18 22:02:55 +0200

    more work work work work work

---
+++ Syndiyo/LandingViewController.swift
@@ -0,0 +1,40 @@
+//
+//  LandingViewController.swift
+//  Syndiyo
+//
+//  Created by Ilham Nurjadin on 7/18/16.
+//  Copyright © 2016 Alan-Yu. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        //self.navigationController?.navigationBarHidden = true
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        //self.navigationController?.navigationBarHidden = false
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Syndiyo/MedicalInformation.swift
+++ Syndiyo/MedicalInformation.swift
@@ -14,19 +14,25 @@ class MedicalInformation {
     var height: Int
     var weight: Int
     var allergies: [String]
+    var previousMedicalConditions: [String]
+    var medications: [String]
     
     init() {
         self.dob = NSDate()
         self.height = 0
         self.weight = 0
         self.allergies = [String]()
+        self.previousMedicalConditions = [String]()
+        self.medications = [String]()
     }
     
-    init(dob: NSDate, height: Int, weight: Int, allergies: [String]) {
+    init(dob: NSDate, height: Int, weight: Int, allergies: [String], previousMedicalConditions: [String], medications: [String]) {
         self.dob = dob
         self.height = height
         self.weight = weight
         self.allergies = allergies
+        self.previousMedicalConditions = previousMedicalConditions
+        self.medications = medications
     }
     
 }
