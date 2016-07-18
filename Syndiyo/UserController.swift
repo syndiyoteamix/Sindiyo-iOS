d77598b6b836f997da23c7e4550ef09b53b616f0
Ilham Nurjadin <ilham1400@gmail.com>
2016-07-18 14:32:07 +0200

    Hey

---
+++ Syndiyo/UserController.swift
@@ -0,0 +1,19 @@
+//
+//  UserController.swift
+//  Syndiyo
+//
+//  Created by Ilham Nurjadin on 7/18/16.
+//  Copyright © 2016 Alan-Yu. All rights reserved.
+//
+
+import Foundation
+
+class UserController {
+    
+    static let sharedInstance = UserController()
+    
+    var currentUser: User = User()
+    
+    
+    
+}
